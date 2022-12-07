a9aafc82501886dc62a84b959c116fd9c0aa4d69
Joana Sanchez <119528221+Joanasn@users.noreply.github.com>
2022-12-07 14:03:55 -0800

    Update Project-DVD rental-PostgreSQL.sql

--- Project-DVD rental-PostgreSQL.sql
+++ Project-DVD rental-PostgreSQL.sql
@@ -1,16 +1,14 @@
 /*
+
 Project Description: DVD rentals. 
 The DVD rental store wants to use the data to know about their customers, their preferences and 
 movements. Having this specific infomation will help the store evaluate about prices, reach to clients
 for specific questions or sales and mainly, know if the strategy they are using so far is working. 
 
-Basic queries such as: select, where, order by, limit, group by, date, having
-
-Tables: actor, address, category, city, country, customer, film, film_actor, film_category, city,
-country, customer, film, film_actor, film_category, inventory, language, payment, rental, staff, store
 
 I will be stating situations as a dvd rental company, from looking up customers, inventory, costs, etc. 
 
+** = Result shown in Tableau
 
 */
 
@@ -72,7 +70,7 @@ WHERE amount >= 5
 ORDER BY amount
 
 
--- How many unqie districts are our customers from?
+-- How many unique districts are our customers from?
 
 SELECT count(DISTINCT(district))
 FROM address
@@ -113,7 +111,7 @@ FROM payment
 GROUP BY staff_id, customer_id
 ORDER BY customer_id, sum(amount)
 
--- How much payments have we received each day?
+**-- How much payments have we received each day?
 
 SELECT DATE(payment_date), SUM(amount)
 FROM payment
@@ -147,3 +145,101 @@ SELECT customer_id, COUNT(amount)
 FROM payment
 GROUP BY customer_id
 HAVING COUNT(amount) >= 40
+
+-- Emails of the customer who live in California
+
+SELECT email, district
+FROM customer
+JOIN address
+ON customer.address_id = address.address_id
+WHERE district ILIKE 'california'
+ORDER BY 1
+
+-- A list of all the movies 'Nick Wahlberg' has been in
+
+SELECT film.title, actor.first_name, actor.last_name
+FROM film
+JOIN film_actor
+ON film.film_id = film_actor.film_id
+JOIN actor
+ON film_actor.actor_id = actor.actor_id
+WHERE actor.first_name ILIKE 'nick'
+AND actor.last_name ILIKE 'wahlberg'
+ORDER BY 1
+
+-- Amount of movies that each customer have rented out?
+
+SELECT rental.customer_id, customer.first_name, customer.last_name, count(rental.customer_id)
+FROM rental
+JOIN customer
+ON rental.customer_id = customer.customer_id
+GROUP BY rental.customer_id, customer.first_name, customer.last_name
+ORDER BY 2
+
+
+-- Who are the customers from USA?
+
+SELECT customer.customer_id, customer.first_name, customer.last_name, city.city, address.district
+FROM customer
+JOIN address
+ON customer.address_id = address.address_id
+JOIN city
+ON address.city_id = city.city_id
+JOIN country
+ON city.country_id = country.country_id
+WHERE country.country ILIKE 'united states'
+ORDER BY 1
+
+-- From how many districts are our customers from?
+
+SELECT address.district, COUNT(district)
+FROM customer
+INNER JOIN address 
+ON customer.address_id = address.address_id
+GROUP BY district
+ORDER BY 1
+
+-- Show how many cities are per country?
+
+SELECT country.country, count(city.country_id)
+FROM city
+INNER JOIN country
+ON city.country_id = country.country_id
+GROUP BY country.country, city.country_id
+
+-- The top 10 countries where our customers are from
+SELECT country.country, count(city.country_id)
+FROM city
+INNER JOIN country
+ON city.country_id = country.country_id
+GROUP BY country.country, city.country_id
+ORDER BY 2 DESC
+LIMIT 10
+
+-- Payment date organized on European standard
+
+SELECT TO_CHAR(payment_date, 'DD-MM-YYYY')
+FROM payment
+
+-- During which months did payments occur? (With the full written month)
+
+SELECT DISTINCT(TO_CHAR(payment_date, 'Month'))
+FROM payment
+
+-- How many payments occured on a Monday?
+
+SELECT COUNT(*)
+FROM payment
+WHERE EXTRACT(dow FROM payment_date) = 1
+
+-- Films that have been returned between 2005-05-29 to 2005-05-30
+
+SELECT film_id, title 
+FROM film
+WHERE film_id IN
+(select inventory.film_id 
+from rental
+INNER JOIN inventory 
+ON inventory.inventory_id = rental.inventory_id
+WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30')
+order by 1
